85823189cfe1a1165521483c452f974756cc6ed9
DimensionR <837629890@qq.com>
2026-04-24 15:56:16 +0800

    refactor(challenge): 删除题目命令仓储旧式无上下文接口

---
+++ code/backend/internal/module/challenge/ports/challenge_command_context_contract_test.go
@@ -0,0 +1,65 @@
+package ports_test
+
+import (
+	"context"
+	"time"
+
+	"ctf-platform/internal/model"
+	challengeports "ctf-platform/internal/module/challenge/ports"
+)
+
+type ctxOnlyChallengeCommandRepository struct{}
+
+func (ctxOnlyChallengeCommandRepository) CreateWithHintsWithContext(context.Context, *model.Challenge, []*model.ChallengeHint) error {
+	return nil
+}
+
+func (ctxOnlyChallengeCommandRepository) FindByIDWithContext(context.Context, int64) (*model.Challenge, error) {
+	return nil, nil
+}
+
+func (ctxOnlyChallengeCommandRepository) UpdateWithContext(context.Context, *model.Challenge) error {
+	return nil
+}
+
+func (ctxOnlyChallengeCommandRepository) UpdateWithHintsWithContext(context.Context, *model.Challenge, []*model.ChallengeHint, bool) error {
+	return nil
+}
+
+func (ctxOnlyChallengeCommandRepository) DeleteWithContext(context.Context, int64) error {
+	return nil
+}
+
+func (ctxOnlyChallengeCommandRepository) HasRunningInstancesWithContext(context.Context, int64) (bool, error) {
+	return false, nil
+}
+
+func (ctxOnlyChallengeCommandRepository) CreatePublishCheckJob(context.Context, *model.ChallengePublishCheckJob) error {
+	return nil
+}
+
+func (ctxOnlyChallengeCommandRepository) FindPublishCheckJobByID(context.Context, int64) (*model.ChallengePublishCheckJob, error) {
+	return nil, nil
+}
+
+func (ctxOnlyChallengeCommandRepository) FindActivePublishCheckJobByChallengeID(context.Context, int64) (*model.ChallengePublishCheckJob, error) {
+	return nil, nil
+}
+
+func (ctxOnlyChallengeCommandRepository) FindLatestPublishCheckJobByChallengeID(context.Context, int64) (*model.ChallengePublishCheckJob, error) {
+	return nil, nil
+}
+
+func (ctxOnlyChallengeCommandRepository) ListPendingPublishCheckJobs(context.Context, int) ([]*model.ChallengePublishCheckJob, error) {
+	return nil, nil
+}
+
+func (ctxOnlyChallengeCommandRepository) TryStartPublishCheckJob(context.Context, int64, time.Time) (bool, error) {
+	return false, nil
+}
+
+func (ctxOnlyChallengeCommandRepository) UpdatePublishCheckJob(context.Context, *model.ChallengePublishCheckJob) error {
+	return nil
+}
+
+var _ challengeports.ChallengeCommandRepository = (*ctxOnlyChallengeCommandRepository)(nil)
