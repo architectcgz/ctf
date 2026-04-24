b9c30c1ec773571a2daeb0188706b3b1785da68e
DimensionR <837629890@qq.com>
2026-04-24 16:08:54 +0800

    refactor(challenge): 删除标签仓储旧式无上下文包装方法

--- code/backend/internal/module/challenge/infrastructure/tag_repository.go
+++ code/backend/internal/module/challenge/infrastructure/tag_repository.go
@@ -23,25 +23,8 @@ func (r *TagRepository) dbWithContext(ctx context.Context) *gorm.DB {
 	return r.db.WithContext(ctx)
 }
 
-func (r *TagRepository) Create(tag *model.Tag) error {
-	return r.CreateWithContext(context.Background(), tag)
-}
-
 func (r *TagRepository) CreateWithContext(ctx context.Context, tag *model.Tag) error {
 	return r.dbWithContext(ctx).Create(tag).Error
-}
-
-func (r *TagRepository) FindByID(id int64) (*model.Tag, error) {
-	var tag model.Tag
-	err := r.db.Where("id = ?", id).First(&tag).Error
-	if err != nil {
-		return nil, err
-	}
-	return &tag, nil
-}
-
-func (r *TagRepository) List(tagType string) ([]*model.Tag, error) {
-	return r.ListWithContext(context.Background(), tagType)
 }
 
 func (r *TagRepository) ListWithContext(ctx context.Context, tagType string) ([]*model.Tag, error) {
@@ -54,35 +37,15 @@ func (r *TagRepository) ListWithContext(ctx context.Context, tagType string) ([]
 	return tags, err
 }
 
-func (r *TagRepository) FindByIDs(ids []int64) ([]*model.Tag, error) {
-	return r.FindByIDsWithContext(context.Background(), ids)
-}
-
 func (r *TagRepository) FindByIDsWithContext(ctx context.Context, ids []int64) ([]*model.Tag, error) {
 	var tags []*model.Tag
 	err := r.dbWithContext(ctx).Where("id IN ?", ids).Find(&tags).Error
 	return tags, err
 }
 
-func (r *TagRepository) AttachToChallenge(challengeID, tagID int64) error {
-	ct := &model.ChallengeTag{
-		ChallengeID: challengeID,
-		TagID:       tagID,
-	}
-	return r.db.Create(ct).Error
-}
-
-func (r *TagRepository) DetachFromChallenge(challengeID, tagID int64) error {
-	return r.DetachFromChallengeWithContext(context.Background(), challengeID, tagID)
-}
-
 func (r *TagRepository) DetachFromChallengeWithContext(ctx context.Context, challengeID, tagID int64) error {
 	return r.dbWithContext(ctx).Where("challenge_id = ? AND tag_id = ?", challengeID, tagID).
 		Delete(&model.ChallengeTag{}).Error
-}
-
-func (r *TagRepository) FindByChallengeID(challengeID int64) ([]*model.Tag, error) {
-	return r.FindByChallengeIDWithContext(context.Background(), challengeID)
 }
 
 func (r *TagRepository) FindByChallengeIDWithContext(ctx context.Context, challengeID int64) ([]*model.Tag, error) {
@@ -93,10 +56,6 @@ func (r *TagRepository) FindByChallengeIDWithContext(ctx context.Context, challe
 		Order("tags.type, tags.name").
 		Find(&tags).Error
 	return tags, err
-}
-
-func (r *TagRepository) AttachTagsInTx(challengeID int64, tagIDs []int64) error {
-	return r.AttachTagsInTxWithContext(context.Background(), challengeID, tagIDs)
 }
 
 func (r *TagRepository) AttachTagsInTxWithContext(ctx context.Context, challengeID int64, tagIDs []int64) error {
@@ -114,16 +73,8 @@ func (r *TagRepository) AttachTagsInTxWithContext(ctx context.Context, challenge
 	})
 }
 
-func (r *TagRepository) Delete(id int64) error {
-	return r.DeleteWithContext(context.Background(), id)
-}
-
 func (r *TagRepository) DeleteWithContext(ctx context.Context, id int64) error {
 	return r.dbWithContext(ctx).Delete(&model.Tag{}, id).Error
-}
-
-func (r *TagRepository) CountChallengesByTagID(tagID int64) (int64, error) {
-	return r.CountChallengesByTagIDWithContext(context.Background(), tagID)
 }
 
 func (r *TagRepository) CountChallengesByTagIDWithContext(ctx context.Context, tagID int64) (int64, error) {
